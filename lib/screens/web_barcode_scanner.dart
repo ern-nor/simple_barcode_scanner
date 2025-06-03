723c8618382932692ea4a2099efc135d5b7f3b85
Ernestas Noreika <ern2221@gmail.com>
2025-06-03 18:07:40 +0200

    update

---
+++ lib/screens/web_barcode_scanner.dart
@@ -0,0 +1,117 @@
+import 'dart:ui_web' as ui;
+
+import 'package:flutter/material.dart';
+import 'package:simple_barcode_scanner/constant.dart';
+import 'package:simple_barcode_scanner/enum.dart';
+// ignore: avoid_web_libraries_in_flutter
+import 'package:web/web.dart' as html;
+
+import '../barcode_appbar.dart';
+import 'barcode_controller.dart';
+
+/// Barcode scanner for web using iframe
+class WebBarcodeScanner extends StatelessWidget {
+  final String lineColor;
+  final String cancelButtonText;
+  final bool isShowFlashIcon;
+  final ScanType scanType;
+  final CameraFace cameraFace;
+  final Function(String) onScanned;
+  final String? appBarTitle;
+  final bool? centerTitle;
+  final Widget? child;
+  final BarcodeAppBar? barcodeAppBar;
+  final int? delayMillis;
+  final Function? onClose;
+  final ScanFormat scanFormat;
+
+  const WebBarcodeScanner({
+    super.key,
+    required this.lineColor,
+    required this.cancelButtonText,
+    required this.isShowFlashIcon,
+    required this.scanType,
+    this.cameraFace = CameraFace.back,
+    required this.onScanned,
+    this.appBarTitle,
+    this.centerTitle,
+    this.child,
+    this.barcodeAppBar,
+    this.delayMillis,
+    this.onClose,
+    this.scanFormat = ScanFormat.ALL_FORMATS,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    String createdViewId = DateTime.now().microsecondsSinceEpoch.toString();
+    String? barcodeNumber;
+
+    final iframe = html.HTMLIFrameElement()
+      ..src = PackageConstant.barcodeFileWebPath
+      ..style.border = 'none'
+      ..style.width = '100%'
+      ..style.height = '100%'
+      ..onLoad.listen((event) async {
+        /// Barcode listener on success barcode scanned
+        html.window.onMessage.listen((event) {
+          /// If barcode is null then assign scanned barcode
+          /// and close the screen otherwise keep scanning
+          if (barcodeNumber == null) {
+            barcodeNumber = event.data.toString();
+            onScanned(barcodeNumber!);
+          }
+        });
+      });
+    // ignore: undefined_prefixed_name
+    ui.platformViewRegistry
+        .registerViewFactory(createdViewId, (int viewId) => iframe);
+
+    final width = MediaQuery.of(context).size.width;
+
+    final height = MediaQuery.of(context).size.height;
+
+    return SizedBox(
+      height: height,
+      width: width,
+      child: HtmlElementView(
+        viewType: createdViewId,
+      ),
+    );
+  }
+}
+
+typedef BarcodeScannerViewCreated = void Function(
+    BarcodeViewController controller);
+
+class BarcodeScannerView extends StatelessWidget {
+  final BarcodeScannerViewCreated onBarcodeViewCreated;
+  final ScanType scanType;
+  final CameraFace cameraFace;
+  final Function(String)? onScanned;
+  final Widget? child;
+  final int? delayMillis;
+  final Function? onClose;
+  final bool continuous;
+  final double? scannerWidth;
+  final double? scannerHeight;
+  final ScanFormat scanFormat;
+  const BarcodeScannerView(
+      {super.key,
+      this.scannerWidth,
+      this.scannerHeight,
+      required this.scanType,
+      this.cameraFace = CameraFace.back,
+      required this.onScanned,
+      this.continuous = false,
+      this.child,
+      this.delayMillis,
+      this.onClose,
+      this.scanFormat = ScanFormat.ALL_FORMATS,
+      required this.onBarcodeViewCreated});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Platform not supported'));
+  }
+}
---
+++ lib/screens/webview_barcode_scanner.dart
@@ -0,0 +1,134 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:path/path.dart' as p;
+import 'package:permission_handler/permission_handler.dart';
+import 'package:simple_barcode_scanner/constant.dart';
+import 'package:simple_barcode_scanner/enum.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+import '../barcode_appbar.dart';
+
+class WebviewBarcodeScanner extends StatelessWidget {
+  final String lineColor;
+  final String cancelButtonText;
+  final bool isShowFlashIcon;
+  final ScanType scanType;
+  final CameraFace cameraFace;
+  final Function(String) onScanned;
+  final String? appBarTitle;
+  final bool? centerTitle;
+  final BarcodeAppBar? barcodeAppBar;
+  final int? delayMillis;
+  final Function? onClose;
+
+  const WebviewBarcodeScanner({
+    super.key,
+    required this.lineColor,
+    required this.cancelButtonText,
+    required this.isShowFlashIcon,
+    required this.scanType,
+    this.cameraFace = CameraFace.back,
+    required this.onScanned,
+    this.appBarTitle,
+    this.centerTitle,
+    this.barcodeAppBar,
+    this.delayMillis,
+    this.onClose,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    WebViewController controller = WebViewController();
+    bool isPermissionGranted = false;
+
+    _checkCameraPermission().then((granted) {
+      debugPrint("Permission is $granted");
+      isPermissionGranted = granted;
+    });
+
+    return FutureBuilder<bool>(
+        future: initPlatformState(
+          controller: controller,
+        ),
+        builder: (context, snapshot) {
+          if (snapshot.hasData && snapshot.data != null) {
+            return WebViewWidget(
+              controller: controller,
+              //permissionRequested: (url, permissionKind, isUserInitiated) =>
+              //     _onPermissionRequested(
+              //   url: url,
+              //   kind: permissionKind,
+              //   isUserInitiated: isUserInitiated,
+              //   context: context,
+              //   isPermissionGranted: isPermissionGranted,
+
+              // ),
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text(snapshot.error.toString()),
+            );
+          }
+
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        });
+  }
+
+  /// Checks if camera permission has already been granted
+  Future<bool> _checkCameraPermission() async {
+    return await Permission.camera.status.isGranted;
+  }
+
+  String getAssetFileUrl({required String asset}) {
+    final assetsDirectory = p.join(p.dirname(Platform.resolvedExecutable),
+        'data', 'flutter_assets', asset);
+    return Uri.file(assetsDirectory).toString();
+  }
+
+  Future<bool> initPlatformState(
+      {required WebViewController controller}) async {
+    String? barcodeNumber;
+
+    try {
+      await controller.loadFlutterAsset(
+          getAssetFileUrl(asset: PackageConstant.barcodeFilePath));
+
+      /// Listen to web to receive barcode
+      // controller.webMessage.listen((event) {
+      //   if (event['methodName'] == "successCallback") {
+      //     if (event['data'] is String &&
+      //         event['data'].isNotEmpty &&
+      //         barcodeNumber == null) {
+      //       barcodeNumber = event['data'];
+      //       onScanned(barcodeNumber!);
+      //     }
+      //   }
+      // });
+
+      await controller.setJavaScriptMode(JavaScriptMode.unrestricted);
+
+      await controller.addJavaScriptChannel('MessageInvoker',
+          onMessageReceived: (JavaScriptMessage message) {
+        final event = jsonDecode(message.message);
+        final methodName = event['methodName'];
+        final data = event['data'];
+
+        if (methodName == "successCallback" &&
+            data is String &&
+            data.isNotEmpty) {
+          barcodeNumber ??= data;
+          if (barcodeNumber != null) {
+            onScanned(barcodeNumber!);
+          }
+        }
+      });
+    } catch (e) {
+      rethrow;
+    }
+    return true;
+  }
+}
